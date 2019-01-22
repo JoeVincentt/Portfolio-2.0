cd4c1a0b77937a1b3d0bb5da038e90d9d149095e
JoeVincentt <eugene.bb@hotmail.com>
2019-01-21 22:49:19 -0500

    project page responsive

--- src/components/Projects/ProjectOne/ProjectOne.js
+++ src/components/Projects/ProjectOne/ProjectOne.js
@@ -23,79 +23,164 @@ const Box = posed.div({
 
 const ProjectOne = () => {
   return (
-    <IconContext.Provider
-      value={{
-        size: "30px",
-        className: "global-class-name"
-      }}
-    >
-      <div className="container center project-container-align">
-        <div className="container white-text">
-          <div className="row">
-            <div className="col m12 s10 offset-s1">
-              <Carousel
-                fixedItem={<h2 className="center">Project name</h2>}
-                images={[
-                  "https://lorempixel.com/800/400/food/1",
-                  "https://lorempixel.com/800/400/food/2",
-                  "https://lorempixel.com/800/400/food/3",
-                  "https://lorempixel.com/800/400/food/4",
-                  "https://lorempixel.com/800/400/food/5"
-                ]}
-              />
-              <div className="card transparent center">
-                <div
-                  className="container center"
-                  style={{ overflow: "hidden" }}
+    // <IconContext.Provider
+    //   value={{
+    //     size: "30px",
+    //     className: "global-class-name"
+    //   }}
+    // >
+    //   <div className="container center project-container-align">
+    //     <div className="container white-text">
+    //       <div className="row">
+    //         <div className="col m12 s10 offset-s1">
+    //           <Carousel
+    //             fixedItem={<h2 className="center">Project name</h2>}
+    //             images={[
+    //               "https://lorempixel.com/800/400/food/1",
+    //               "https://lorempixel.com/800/400/food/2",
+    //               "https://lorempixel.com/800/400/food/3",
+    //               "https://lorempixel.com/800/400/food/4",
+    //               "https://lorempixel.com/800/400/food/5"
+    //             ]}
+    //           />
+    //           <div className="card transparent center">
+    //             <div
+    //               className="container center"
+    //               style={{ overflow: "hidden" }}
+    //             />
+    //             <div className="container center">
+    //               <div className="card-tabs center">
+    //                 <div className="col m12 s10 offset-s1 offset-m2">
+    //                   <ul
+    //                     className="tabs tabs-fixed-width center margin-tab-md transparent"
+    //                     style={{ overflow: "visible" }}
+    //                   >
+    //                     <Box>
+    //                       <li
+    //                         className="tab center"
+    //                         style={{ zIndex: "2000", marginLeft: "15px" }}
+    //                       >
+    //                         <a href="#Description" className="active">
+    //                           <img
+    //                             src="images/Projects/info.png"
+    //                             alt="Description"
+    //                             style={{ width: "45px" }}
+    //                           />
+    //                         </a>
+    //                       </li>
+    //                     </Box>
+    //                     <Box>
+    //                       <li className="tab">
+    //                         <a href="#Technology">
+    //                           <img
+    //                             src="images/Projects/tools.png"
+    //                             alt="Technology"
+    //                             style={{ height: "40px" }}
+    //                           />
+    //                         </a>
+    //                       </li>{" "}
+    //                     </Box>
+    //                     <Box>
+    //                       <li className="tab">
+    //                         <a href="#Github">
+    //                           <img
+    //                             src="images/Projects/github.png"
+    //                             alt="Github"
+    //                             style={{ height: "45px" }}
+    //                           />
+    //                         </a>
+    //                       </li>{" "}
+    //                     </Box>
+    //                   </ul>
+    //                 </div>
+    //               </div>
+    //               <div className="card-content transparent center lighten-4">
+    //                 <div className="container center">
+    //                   <div id="Description">Description</div>
+    //                   <div id="Technology">Technology</div>
+    //                   <div id="Github">
+    //                     <h5>Check out code </h5>
+    //                     <a
+    //                       className="pulsate-fwd"
+    //                       href="https://github.com/JoeVincentt"
+    //                       rel="noopener noreferrer"
+    //                       target="_blank"
+    //                     >
+    //                       {" "}
+    //                       <FaArrowAltCircleRight className="pulsate-fwd" />
+    //                     </a>
+    //                   </div>
+    //                 </div>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </IconContext.Provider>
+    <div className="container center-align" style={{ marginTop: "-200px" }}>
+      <IconContext.Provider
+        value={{
+          size: "30px",
+          className: "global-class-name"
+        }}
+      >
+        <div className="container">
+          <div className="container  white-text">
+            <div className="row">
+              <div className="col m12 s12 l12">
+                <Carousel
+                  className="center-align"
+                  fixedItem={<h2 className="center">Project name</h2>}
+                  images={[
+                    "https://lorempixel.com/800/400/food/1",
+                    "https://lorempixel.com/800/400/food/2",
+                    "https://lorempixel.com/800/400/food/3",
+                    "https://lorempixel.com/800/400/food/4",
+                    "https://lorempixel.com/800/400/food/5"
+                  ]}
                 />
-                <div className="container center">
-                  <div className="card-tabs center">
-                    <div className="col m12 s10 offset-s1 offset-m2">
-                      <ul
-                        className="tabs tabs-fixed-width center margin-tab-md transparent"
-                        style={{ overflow: "visible" }}
-                      >
-                        <Box>
-                          <li
-                            className="tab center"
-                            style={{ zIndex: "2000", marginLeft: "15px" }}
-                          >
-                            <a href="#Description" className="active">
-                              <img
-                                src="images/Projects/info.png"
-                                alt="Description"
-                                style={{ width: "45px" }}
-                              />
-                            </a>
-                          </li>
-                        </Box>
-                        <Box>
-                          <li className="tab">
-                            <a href="#Technology">
-                              <img
-                                src="images/Projects/tools.png"
-                                alt="Technology"
-                                style={{ height: "40px" }}
-                              />
-                            </a>
-                          </li>{" "}
-                        </Box>
-                        <Box>
-                          <li className="tab">
-                            <a href="#Github">
-                              <img
-                                src="images/Projects/github.png"
-                                alt="Github"
-                                style={{ height: "45px" }}
-                              />
-                            </a>
-                          </li>{" "}
-                        </Box>
-                      </ul>
-                    </div>
+                <div className="container transparent center-align center ">
+                  <div className="card-tabs center-align offset-m4 project-container-align">
+                    <ul className="tabs tabs-fixed-width center-align center  transparent">
+                      <Box>
+                        <li className="tab center">
+                          <a href="#Description" className="active">
+                            <img
+                              src="images/Projects/info.png"
+                              alt="Description"
+                              style={{ width: "45px" }}
+                            />
+                          </a>
+                        </li>
+                      </Box>
+                      <Box>
+                        <li className="tab">
+                          <a href="#Technology">
+                            <img
+                              src="images/Projects/tools.png"
+                              alt="Technology"
+                              style={{ height: "40px" }}
+                            />
+                          </a>
+                        </li>{" "}
+                      </Box>
+                      <Box>
+                        <li className="tab">
+                          <a href="#Github">
+                            <img
+                              src="images/Projects/github.png"
+                              alt="Github"
+                              style={{ height: "45px" }}
+                            />
+                          </a>
+                        </li>{" "}
+                      </Box>
+                    </ul>
                   </div>
                   <div className="card-content transparent center lighten-4">
-                    <div className="container center">
+                    <div className="center-align">
                       <div id="Description">Description</div>
                       <div id="Technology">Technology</div>
                       <div id="Github">
@@ -117,8 +202,8 @@ const ProjectOne = () => {
             </div>
           </div>
         </div>
-      </div>
-    </IconContext.Provider>
+      </IconContext.Provider>
+    </div>
   );
 };
 
