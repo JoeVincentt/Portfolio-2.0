6af6e507a62fa98a69e4e53b27462f1ba40f0b27
JoeVincentt <eugene.bb@hotmail.com>
2019-01-22 22:25:24 -0500

    finished About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,59 +18,85 @@ class About extends Component {
 
   render() {
     return (
-      <IconContext.Provider
-        value={{
-          size: "30px",
-          className: "global-class-name"
-        }}
-      >
-        <div className="container center white-text">
-          <div className="row">
-            <div className="col s7 m6 offset-m3 offset-s2 center">
-              <Tilt
-                className="Tilt"
-                options={{ max: 25 }}
-                style={{ height: 450, width: "auto" }}
-              >
-                <div className="Tilt-inner">
-                  {" "}
-                  <div
-                    className="card  transparent white-text center z-depth-0"
-                    onMouseEnter={this.onMouseEnter}
-                    onMouseLeave={this.onMouseLeave}
+      <div className="container center-align white-text">
+        <div className="row">
+          <div className="col s12 m12 l12">
+            <div
+              className="col m1 l1 hide-on-small-only "
+              style={{
+                overflow: "visible",
+                marginTop: "60px",
+                fontStyle: "italic",
+                color: "grey"
+              }}
+            >
+              <h4>A</h4>
+              <h4>B</h4>
+              <h4>O</h4>
+              <h4>U</h4>
+              <h4>T</h4>
+            </div>
+            <div className="col s12 m11 l11">
+              <div className="row center">
+                <div className="col s10 m6 offset-m3 offset-s1 center ">
+                  <IconContext.Provider
+                    value={{
+                      size: "20%",
+                      className: "global-class-name"
+                    }}
                   >
-                    <div className="card-image">
-                      {" "}
-                      <img
-                        src={avatar}
-                        alt="avatar"
-                        style={{ width: "100%" }}
-                      />{" "}
-                    </div>
-                  </div>
-                  <div className="card-stacked">
-                    <div className="card-content">
-                      <span>
-                        <ReactRevealText threshold={0.4} show={this.state.show}>
-                          I am a very simple card. I am good at containing small
-                          bits of information.I am a very simple card. I am good
-                          at containing small bits of information.I am a very
-                          simple card. I am good at containing small bits of
-                          information.I am a very simple card. I am good at
-                          containing small bits of information.I am a very
-                          simple card. I am good at containing small bits of
-                          information.
-                        </ReactRevealText>
-                      </span>
-                    </div>
-                    <div className="card-action" />
-                  </div>
+                    <Tilt
+                      className="Tilt"
+                      options={{ max: 25 }}
+                      style={{ height: 450, width: "auto" }}
+                    >
+                      <div className="Tilt-inner">
+                        {" "}
+                        <div
+                          className="card  transparent white-text center z-depth-0"
+                          onMouseEnter={this.onMouseEnter}
+                          onMouseLeave={this.onMouseLeave}
+                        >
+                          <div className="card-image">
+                            {" "}
+                            <img
+                              src={avatar}
+                              alt="avatar"
+                              style={{ width: "100%" }}
+                            />{" "}
+                          </div>
+                        </div>
+                        <div className="card-stacked">
+                          <div className="card-content">
+                            <span>
+                              <ReactRevealText
+                                threshold={0.4}
+                                show={this.state.show}
+                              >
+                                Since first time typed in "Hello World" in
+                                Python, while taking very first course in
+                                programming "Python: for absolute beginners", I
+                                fell in love with programming. It was a quite
+                                long way till I got to Web Development, i
+                                certenely knew I love doing this, but so many
+                                ways to use programming... Learned very basics
+                                of few languages and decided to stay in Web Dev.
+                                Something what we use everyday and something
+                                people can see and appreciate if its done right!
+                              </ReactRevealText>
+                            </span>
+                          </div>
+                          <div className="card-action" />
+                        </div>
+                      </div>
+                    </Tilt>
+                  </IconContext.Provider>
                 </div>
-              </Tilt>
+              </div>
             </div>
           </div>
         </div>
-      </IconContext.Provider>
+      </div>
     );
   }
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -20,160 +20,195 @@ class Skills extends Component {
   render() {
     return (
       <div className="container center white-text">
-        <div className="row center">
-          <div className="col m12 s12 l12 ">
-            <div className="card transparent z-depth-0">
-              <div className="container">
-                <div className="card-content">
-                  <div className="row ">
-                    <div className="col s6 m4 l4 offset-l2 offset-m1">
-                      <ul className="center-align">
-                        <li>Problem solving</li>
-                        <li>Problem solving</li>
-                        <li>Problem solving</li>
-                        <li>Problem solving</li>
-                      </ul>
-                    </div>
-                    <div className="col s6 m4 l4 offset-l1 offset-m2">
-                      <ul className="left-align">
-                        <li>Problem solving</li>
-                        <li>Problem solving</li>
-                        <li>Problem solving</li>
-                        <li>Problem solving</li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="divider" />
-                  <div className="container center">
-                    <div className="row">
-                      <div className="col s6 m6 l6">
-                        <ul
-                          className="transparent center row z-depth-0"
-                          style={{ overflow: "visible" }}
-                        >
-                          <div className="valign-wrapper">
-                            {" "}
-                            <div className="container">
-                              {" "}
-                              <Box>
-                                <li className="">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/mongodbc.png"
-                                    alt="MongoDB"
-                                    style={{ height: "40px" }}
-                                  />
-                                </li>{" "}
-                              </Box>
-                              <Box>
-                                <li className="">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/nodejs.png"
-                                    alt="NodeJS"
-                                    style={{ height: "45px" }}
-                                  />
-                                </li>{" "}
-                              </Box>
-                              <Box>
-                                <li className="">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/graphql.png"
-                                    alt="GraphQL"
-                                    style={{ height: "40px" }}
-                                  />
-                                </li>{" "}
-                              </Box>{" "}
-                              <Box>
-                                <li className="z-depth-0">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/python.png"
-                                    alt="python"
-                                    style={{ height: "60px", margin: "-10px" }}
-                                  />
-                                </li>
-                              </Box>
-                              <Box>
-                                <li className="">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/git.png"
-                                    alt="git"
-                                    style={{ height: "40px" }}
-                                  />
-                                </li>{" "}
-                              </Box>{" "}
+        <div className="row">
+          <div className="col s12 m12 l12">
+            <div
+              className="col m1 l1 hide-on-small-only "
+              style={{
+                overflow: "visible",
+                marginTop: "20%",
+                fontStyle: "italic",
+                color: "grey"
+              }}
+            >
+              <h4>S</h4>
+              <h4>K</h4>
+              <h4>I</h4>
+              <h4>L</h4>
+              <h4>L</h4>
+              <h4>S</h4>
+            </div>
+            <div className="col s12 m11 l11">
+              <div className="row center">
+                <div className="col m12 s12 l12 ">
+                  {" "}
+                  <div className="card transparent z-depth-0">
+                    <div className="container">
+                      <div className="card-content">
+                        <div className="row ">
+                          <div className="center">
+                            <h4>Soft:</h4>
+                          </div>
+                          <div className="col s6 m4 l4 offset-l2 offset-m1">
+                            <ul className="center-align">
+                              <li>Problem solving</li>
+                              <li>Empathy</li>
+                              <li>Communication</li>
+                              <li>Teamwork</li>
+                            </ul>
+                          </div>
+                          <div className="col s6 m4 l4  offset-m2">
+                            <ul className="center">
+                              <li>Patience</li>
+                              <li>Open-mindedness</li>
+                              <li>Accountability</li>
+                              <li>Approachability</li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div className="divider" />
+                        <div className="container center">
+                          <div className="row">
+                            <div className="center">
+                              <h4>Technical:</h4>
                             </div>
-                          </div>{" "}
-                        </ul>
-                      </div>
-                      <div className="col s6 m6 l6">
-                        <ul
-                          className="transparent center row z-depth-0"
-                          style={{ overflow: "visible" }}
-                        >
-                          <div className="valign-wrapper">
-                            {" "}
-                            <div className="container">
-                              {" "}
-                              <Box>
-                                <li className="z-depth-0">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/react.png"
-                                    alt="ReactJS"
-                                    style={{ height: "45px" }}
-                                  />
-                                </li>
-                              </Box>
-                              <Box>
-                                <li className="z-depth-0">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/redux.png"
-                                    alt="redux"
-                                    style={{ height: "40px" }}
-                                  />
-                                </li>
-                              </Box>
-                              <Box>
-                                <li className="">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/js.png"
-                                    alt="js"
-                                    style={{ height: "40px" }}
-                                  />
-                                </li>{" "}
-                              </Box>
-                              <Box>
-                                <li className="">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/materialize.png"
-                                    alt="materialize"
-                                    style={{ width: "45px", margin: "5px" }}
-                                  />
-                                </li>{" "}
-                              </Box>{" "}
-                              <Box>
-                                <li className="">
-                                  <img
-                                    className="skills-img"
-                                    src="images/Skills/css.png"
-                                    alt="css"
-                                    style={{
-                                      width: "70px",
-                                      marginTop: "-10px"
-                                    }}
-                                  />
-                                </li>{" "}
-                              </Box>
+                            <div className="col s6 m6 l6">
+                              <ul
+                                className="transparent center row z-depth-0"
+                                style={{ overflow: "visible" }}
+                              >
+                                <div className="valign-wrapper">
+                                  {" "}
+                                  <div className="container">
+                                    {" "}
+                                    <Box>
+                                      <li className="">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/mongodbc.png"
+                                          alt="MongoDB"
+                                          style={{ height: "40px" }}
+                                        />
+                                      </li>{" "}
+                                    </Box>
+                                    <Box>
+                                      <li className="">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/nodejs.png"
+                                          alt="NodeJS"
+                                          style={{ height: "45px" }}
+                                        />
+                                      </li>{" "}
+                                    </Box>
+                                    <Box>
+                                      <li className="">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/graphql.png"
+                                          alt="GraphQL"
+                                          style={{ height: "40px" }}
+                                        />
+                                      </li>{" "}
+                                    </Box>{" "}
+                                    <Box>
+                                      <li className="z-depth-0">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/python.png"
+                                          alt="python"
+                                          style={{
+                                            height: "60px",
+                                            margin: "-10px"
+                                          }}
+                                        />
+                                      </li>
+                                    </Box>
+                                    <Box>
+                                      <li className="">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/git.png"
+                                          alt="git"
+                                          style={{ height: "40px" }}
+                                        />
+                                      </li>{" "}
+                                    </Box>{" "}
+                                  </div>
+                                </div>{" "}
+                              </ul>
                             </div>
-                          </div>{" "}
-                        </ul>
+                            <div className="col s6 m6 l6">
+                              <ul
+                                className="transparent center row z-depth-0"
+                                style={{ overflow: "visible" }}
+                              >
+                                <div className="valign-wrapper">
+                                  {" "}
+                                  <div className="container">
+                                    {" "}
+                                    <Box>
+                                      <li className="z-depth-0">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/react.png"
+                                          alt="ReactJS"
+                                          style={{ height: "45px" }}
+                                        />
+                                      </li>
+                                    </Box>
+                                    <Box>
+                                      <li className="z-depth-0">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/redux.png"
+                                          alt="redux"
+                                          style={{ height: "40px" }}
+                                        />
+                                      </li>
+                                    </Box>
+                                    <Box>
+                                      <li className="">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/js.png"
+                                          alt="js"
+                                          style={{ height: "40px" }}
+                                        />
+                                      </li>{" "}
+                                    </Box>
+                                    <Box>
+                                      <li className="">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/materialize.png"
+                                          alt="materialize"
+                                          style={{
+                                            width: "45px",
+                                            margin: "5px"
+                                          }}
+                                        />
+                                      </li>{" "}
+                                    </Box>{" "}
+                                    <Box>
+                                      <li className="">
+                                        <img
+                                          className="skills-img"
+                                          src="images/Skills/css.png"
+                                          alt="css"
+                                          style={{
+                                            width: "70px",
+                                            marginTop: "-10px"
+                                          }}
+                                        />
+                                      </li>{" "}
+                                    </Box>
+                                  </div>
+                                </div>{" "}
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
